a4936b93c5e07fdd7712170992845f946ba3a3e3
AngelesT85 <brawl-ford123456@mail.ru>
2024-09-05 17:57:40 +0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -18,8 +18,21 @@ class Field:
                         print(*["---", "---", "---"], sep="|", end="")
                         print(" ┃ " if LittleLine != 2 else "\n", end="")
             print("━" * 39 if BigLine != 2 else "")
+    
+    def attack(self, coords_big_field, coords_mini_field, who_attack):
+        big_x, big_y = coords_big_field
+        mini_x, mini_y = coords_mini_field
+        self.field[big_y][big_x][mini_x][mini_y] = who_attack
+
+    def IsLiveMiniField(field):
+        pass
+    
+    def TestEndGame(self):
+        pass
+
 
 field = Field()
+field.attack((0, 1), (0, 0), "X")
 field.PrintFieldConsole() 
 
 # Example of print field in terminal
