9415a29da0879ca561a1e77a8ec23016e0c0b9fb
AngelesT85 <brawl-ford123456@mail.ru>
2024-09-06 17:30:37 +0300

    Update images.py

--- loads/images.py
+++ loads/images.py
@@ -4,7 +4,11 @@ def Load(img):
     return load(f"images/{img}.png")
 
 FieldImg = Load("Field")
+
 TicImg = Load("Tic")
 TacToeImg = Load("TacToe")
+BigTicImg = Load("BigTic")
+BigTacToeImg = Load("BigTacToe")
+
 StrokeBlueImg = Load("StrokeBlue")
 StrokeWhiteImg = Load("StrokeWhite")
