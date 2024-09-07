6282855abab5769fce106c95621c5e5df5f2ffd6
AngelesT85 <brawl-ford123456@mail.ru>
2024-09-07 19:06:04 +0300

    Update settings.py

--- loads/settings.py
+++ loads/settings.py
@@ -8,13 +8,16 @@ with open("settings.json", "r", encoding="utf-8") as settings:
     UserFirstMove = settings["User first move"]
     if UserFigure == "o":
         PGfigures = (TacToeImg, TicImg)
+        PGBigFigures = (BigTacToeImg, BigTicImg)
         figures = ("o", "x")
     else:
         PGfigures = (TicImg, TacToeImg)
+        PGBigFigures = (BigTicImg, BigTacToeImg)
         figures = ("x", "o")
 
     if not UserFirstMove:
         PGfigures = PGfigures[::-1]
+        PGBigFigures = PGBigFigures[::-1]
         figures = figures[::-1]
 
 
